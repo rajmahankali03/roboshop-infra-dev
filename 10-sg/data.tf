da06b1d9c3a4e44d86a157dcc8cf31cc1236d91c
RajMahankali <rajmahankali@gmail.com>
2025-08-22 21:52:50 +0530

    mess

--- 10-sg/data.tf
+++ 10-sg/data.tf
@@ -1,3 +1,3 @@
-data "aws_ssm_parameter" "vpc_id" {    # Fetching the VPC ID from SSM Parameter Store  
+data "aws_ssm_parameter" "vpc_id" {    # Fetching the VPC ID from SSM Parameter Store which was created in 00-vpc parameters.tf
   name = "/${var.project}/${var.environment}/vpc_id"
 }
